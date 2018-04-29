1be8d6c89ae6245ae9364bc262b9aa24787e5e88
Narayana Swamy <knswamy@gmail.com>
2018-04-29 17:10:30 -0500

    Add files via upload

--- dllib/model.py
+++ dllib/model.py
@@ -24,6 +24,48 @@ def predict_with_targs_(m, dl):
 def predict_with_targs(m, dl):
     preda,targa = predict_with_targs_(m, dl)
     return to_np(torch.cat(preda)), to_np(torch.cat(targa))
-    
 
+# From https://github.com/ncullen93/torchsample
+def model_summary(m, input_size):
+    def register_hook(module):
+        def hook(module, input, output):
+            class_name = str(module.__class__).split('.')[-1].split("'")[0]
+            module_idx = len(summary)
 
+            m_key = '%s-%i' % (class_name, module_idx+1)
+            summary[m_key] = OrderedDict()
+            summary[m_key]['input_shape'] = list(input[0].size())
+            summary[m_key]['input_shape'][0] = -1
+            #if is_listy(output):
+            #    summary[m_key]['output_shape'] = [[-1] + list(o.size())[1:] for o in output]
+            #else:
+            #    summary[m_key]['output_shape'] = list(output.size())
+            #    summary[m_key]['output_shape'][0] = -1
+            summary[m_key]['output_shape'] = list(output.size())
+            summary[m_key]['output_shape'][0] = -1
+
+            params = 0
+            if hasattr(module, 'weight'):
+                params += torch.prod(torch.LongTensor(list(module.weight.size())))
+                summary[m_key]['trainable'] = module.weight.requires_grad
+            if hasattr(module, 'bias') and module.bias is not None:
+                params +=  torch.prod(torch.LongTensor(list(module.bias.size())))
+            summary[m_key]['nb_params'] = params
+
+        if (not isinstance(module, nn.Sequential) and
+           not isinstance(module, nn.ModuleList) and
+           not (module == m)):
+            hooks.append(module.register_forward_hook(hook))
+
+    summary = OrderedDict()
+    hooks = []
+    m.apply(register_hook)
+
+    #if is_listy(input_size[0]):
+    #    x = [to_gpu(Variable(torch.rand(3,*in_size))) for in_size in input_size]
+    #else: x = [to_gpu(Variable(torch.rand(3,*input_size)))]
+    x = [(Variable(torch.rand(3,*input_size)))]
+    m(*x)
+
+    for h in hooks: h.remove()
+    return summary    
